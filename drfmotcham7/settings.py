bef4349fab7e1c0f16da74b79378bc1612bb65d7
Jung ui heon <ky7662@naver.com>
2023-03-18 21:23:15 +0900

    Sdfsd

--- drfmotcham7/settings.py
+++ drfmotcham7/settings.py
@@ -193,7 +193,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
