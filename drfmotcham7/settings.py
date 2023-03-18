839ec9a597b7aff447b259cd5efc5919f3d2fa72
Jung ui heon <ky7662@naver.com>
2023-03-18 16:19:36 +0900

    fdsfd

--- drfmotcham7/settings.py
+++ drfmotcham7/settings.py
@@ -198,7 +198,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         #'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_RENDERER_CLASSES': [
