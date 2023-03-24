08284882fc4fa4c5787a474c89e7e06e925d53fd
Jung ui heon <ky7662@naver.com>
2023-03-24 17:21:53 +0900

    last

--- myfolders/models.py
+++ myfolders/models.py
@@ -6,8 +6,9 @@ from django.contrib.postgres.fields import ArrayField
 class Folder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
     folder_name = models.CharField(max_length=100)
-    #sender = ArrayField(models.CharField(max_length=100), blank=True, null=True)
-    sender = models.CharField(max_length=100)
-    #keyword = ArrayField(models.CharField(max_length=100), blank=True, null=True)
-    keyword = models.CharField(max_length=100)
-    
+    sender = ArrayField(models.CharField(max_length=100), blank=True, null=True)
+    #sender = models.ListField(models.CharField(max_length=100))
+    #sender = models.CharField(max_length=100)
+    keyword = ArrayField(models.CharField(max_length=100), blank=True, null=True)
+    #keyword = models.ListField(models.CharField(max_length=100))
+    #keyword = models.CharField(max_length=100)
