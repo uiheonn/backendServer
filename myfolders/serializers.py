5b7d884d17cdc7c5c5361f27cd738d970ecff18a
Jung ui heon <ky7662@naver.com>
2023-03-25 17:06:47 +0900

    drf

--- myfolders/serializers.py
+++ myfolders/serializers.py
@@ -15,6 +15,19 @@ def arrayfilter(txt):
     a.append(txt[j:i])
     return a
 
+def arrayfilter2(txt):
+    a =[]
+    i = 2
+    j = 2
+    n = len(txt)
+    while i < n-2:
+        if txt[i] == ",":
+            a.append(txt[j:i])
+            j = i + 1
+        i+=1
+    a.append(txt[j:i])
+    return a
+
 class FolderSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -47,10 +60,15 @@ class FolderSerializer(serializers.ModelSerializer):
         re_res = '{' + ''.join(res).replace('{', '').replace('}', '') + '}'
         tmp = data['keyword']
         re_tmp = '{' + ''.join(tmp).replace('{', '').replace('}', '') + '}'
-        resres = arrayfilter(re_res)
-        tmptmp = arrayfilter(re_tmp)
+        if '{"' in re_res:
+            resres = arrayfilter2(re_res)
+            tmptmp = arrayfilter2(re_tmp)
+        else:
+            resres = arrayfilter(re_res)
+            tmptmp = arrayfilter(re_tmp)
 
-        
+        print(resres)
+        print(tmptmp)
         data['sender'] = resres
         data['keyword'] = tmptmp
         return data
